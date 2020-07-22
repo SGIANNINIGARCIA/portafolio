f5689ea8876a60e0f024c56a7b69ea4adba588c0
Sandro <sgianninigarcia@rollins.edu>
2020-07-22 17:08:57 -0400

    Updates

---
+++ precache-manifest.af39940ae950b3a9d10036ce3cdf81a5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b38524291f7ee02838fa11376323a6d",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "acc6fbe693a1da82fa75",
+    "url": "/portafolio/static/css/main.f34752b9.chunk.css"
+  },
+  {
+    "revision": "0a2c8ba0d7bc0c18e7f9",
+    "url": "/portafolio/static/js/2.e4ec2bd6.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portafolio/static/js/2.e4ec2bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc6fbe693a1da82fa75",
+    "url": "/portafolio/static/js/main.d0f1f360.chunk.js"
+  },
+  {
+    "revision": "1d20c80e54c4c0285f95",
+    "url": "/portafolio/static/js/runtime-main.625dcbe2.js"
+  },
+  {
+    "revision": "afd029812cc76203e841fa347d8235bc",
+    "url": "/portafolio/static/media/boids.afd02981.png"
+  },
+  {
+    "revision": "35bed3581dd53a8fa85a224c7bd96c69",
+    "url": "/portafolio/static/media/interpreter.35bed358.png"
+  },
+  {
+    "revision": "ee5fbb2faf793dae0370cf3a07211ab7",
+    "url": "/portafolio/static/media/kruskal.ee5fbb2f.png"
+  },
+  {
+    "revision": "b35d31be71f7256c80990e9941dc8c46",
+    "url": "/portafolio/static/media/mmc.b35d31be.png"
+  },
+  {
+    "revision": "cfd5e837df8f458ce0e65dc6dd8e4dc4",
+    "url": "/portafolio/static/media/othersandro.cfd5e837.jpg"
+  },
+  {
+    "revision": "5125952bebbfe4c032afff5dda2df4de",
+    "url": "/portafolio/static/media/quickscan.5125952b.png"
+  },
+  {
+    "revision": "c9f570386d59f97cc7065b38f049eb45",
+    "url": "/portafolio/static/media/quickscanapi.c9f57038.png"
+  },
+  {
+    "revision": "b7d2dbc91b1edf53584e647e2a64f39a",
+    "url": "/portafolio/static/media/raytracer.b7d2dbc9.png"
+  },
+  {
+    "revision": "263ab02cc44f374f6b9709aa9c715aa9",
+    "url": "/portafolio/static/media/regime.263ab02c.jpg"
+  },
+  {
+    "revision": "b12d80ed83323fe4f1ee2c89d1787c20",
+    "url": "/portafolio/static/media/sandro.b12d80ed.jpg"
+  },
+  {
+    "revision": "7d3f0ed0d9bccd9ddbf4caafcd970e33",
+    "url": "/portafolio/static/media/shakespeare.7d3f0ed0.jpg"
+  },
+  {
+    "revision": "85663254e6bf6adb54e6a5ca4f7c8def",
+    "url": "/portafolio/static/media/shell.85663254.png"
+  },
+  {
+    "revision": "06b3c8403d4d3e56b2e22b212de051cc",
+    "url": "/portafolio/static/media/snake.06b3c840.png"
+  }
+]);
